595349f85d1dd415ea654e749e0d6b4ff68c73d8
Dillon Draper <dillonbdraper@gmail.com>
2021-01-12 15:55:10 -0600

    added PUT/edit functionality for animals in the server.  Need to review all of this code for comprehension/mastery but I am feeling slightly better.

--- request_handler.py
+++ request_handler.py
@@ -1,12 +1,10 @@
 from locations.request import create_location
 from customers.request import create_customer
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal, delete_animal
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from locations import get_all_locations, get_single_location, create_location, delete_location
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer
 import json
-
-
 
 
 # Here's a class. It inherits from another class.
@@ -49,7 +47,7 @@ class HandleRequests(BaseHTTPRequestHandler):
                 response = f"{get_all_animals()}"
 
         if resource == "locations":
-            if id is not None: 
+            if id is not None:
                 response = f"{get_single_location(id)}"
 
             else:
@@ -88,20 +86,32 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         if resource == "customers":
             new_resource == create_customer(post_body)
-        
+
         if resource == "locations":
             new_resource == create_location(post_body)
 
         # Encode the new animal and send in response
-        
-        self.wfile.write(f"{new_resource}".encode())
 
+        self.wfile.write(f"{new_resource}".encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
 
     def do_PUT(self):
-        self.do_POST()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     def do_DELETE(self):
         # Set a 204 response code
